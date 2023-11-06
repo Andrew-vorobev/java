60d0ac6c21adcc92935154184c2e2f4e899d143d
Andrew-vorobev <43858194+Andrew-vorobev@users.noreply.github.com>
2023-11-06 18:37:50 +0500

    Add files via upload

---
+++ skillbox/mod5/src/main/java/com/example/MyBookShopApp/data/Book.java
@@ -0,0 +1,107 @@
+package com.example.MyBookShopApp.data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private Date pubDate;
+
+    @Column
+    private short isBestseller;
+    @Column
+    private String slug;
+    @Column
+    private String title;
+    @Column
+    private String image;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    @Column
+    private Integer price;
+    @Column
+    private short discount;
+
+
+    public Book() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(Date pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    public short getIsBestseller() {
+        return isBestseller;
+    }
+
+    public void setIsBestseller(short isBestseller) {
+        this.isBestseller = isBestseller;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public short getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(short discount) {
+        this.discount = discount;
+    }
+}
