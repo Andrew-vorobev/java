aba43b52fc70599eea375e58445919925f9a62ae
Andrew-vorobev <43858194+Andrew-vorobev@users.noreply.github.com>
2023-11-19 03:37:25 +0500

    Add files via upload

---
+++ skillbox/mod6/target/classes/dbinsert/tag.sql
@@ -0,0 +1,10 @@
+insert into tag (id, name, slug) values (1, 'Bigtax', 'ba352e7e-2a55-41cf-95b7-205b6ae6f8d0');
+insert into tag (id, name, slug) values (2, 'Voltsillam', '8238fcdd-e652-4a11-8912-3615a462cb5e');
+insert into tag (id, name, slug) values (3, 'Voltsillam', 'd8e8003a-a18b-4b01-9dc6-e02721501bb6');
+insert into tag (id, name, slug) values (4, 'Vagram', 'dad9d63e-4980-4401-b2ae-3f019c1ffb33');
+insert into tag (id, name, slug) values (5, 'Duobam', 'efb560f0-2e07-41f0-9f57-ccc7931b636f');
+insert into tag (id, name, slug) values (6, 'Y-Solowarm', '292ba527-93fa-4e32-ae36-e823aa84e2c8');
+insert into tag (id, name, slug) values (7, 'Sonsing', '4da85237-3026-4b66-bbf0-f8fee5a35305');
+insert into tag (id, name, slug) values (8, 'Zontrax', 'bb91fc43-3c0c-43c3-b1e3-f9b0673c3309');
+insert into tag (id, name, slug) values (9, 'Tin', '4e5e7946-6904-434e-90e7-f8d8d5cbbb9f');
+insert into tag (id, name, slug) values (10, 'Fintone', 'f42cad95-5f45-44ae-b2d1-33f1ac112361');
